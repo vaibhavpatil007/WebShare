7a8456f4513d007f3dea4953a612d0ab548a31f1
vaibhavpatil007 <vpatil2611911@gmail.com>
2025-05-08 10:51:30 +0530

    Added collectstatic file and update settings for adding statis_root

--- webshare/webshare/settings.py
+++ webshare/webshare/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
